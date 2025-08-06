ae7b60c8e046f7666c465897e9492b22a5606cd9
PauloSsF24 <pssf23tc@gmail.com>
2025-08-06 19:37:41 -0300

    Estrutura inicial da landing page da FourFoots com Next.js e TailwindCSS

---
+++ src/app/_components/About.tsx
@@ -0,0 +1,99 @@
+import Image from "next/image";
+import about1Img from '../../../public/dog-about1.jpg'
+import about2Img from '../../../public/dog-about2.jpg'
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
+import { Poppins } from 'next/font/google'
+
+const PoppinsFont = Poppins({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+export function About(){
+    return(
+
+        <section className={`bg-[#FDF6ec] py-16 ${PoppinsFont.className}`}>
+            
+            <div className="container px-4 mx-auto">
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 item-center">
+
+                    <div className="relative">
+                        <div className="relative w-full h-[400px] rounded-3x1 overflow-hidden">
+                            <Image
+                                src={about1Img}
+                                alt="Foto do cachorro"
+                                fill
+                                quality={100}
+                                className="object-cover hover:scale-110 duration-300"
+                                priority
+                            />
+                        </div>
+                        
+                        <div className="absolute w-40 h-40 right-4 -bottom-8 border-4 overflow-hidden border-white rounded-lg ">
+                            <Image
+                                src={about2Img}
+                                alt="Foto do cachorro"
+                                fill
+                                quality={100}
+                                className="object-cover hover:scale-110 duration-300"
+                                priority
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="space-y-6 mt-10">
+                        <h2 className="text-6xl font-bold">🐾 Sobre a FourFoots</h2>
+
+                        <p>
+                            Na FourFoots, acreditamos que todo pet merece amor, cuidado e momentos felizes. Somos um petshop criado por quem ama animais, para quem ama animais. Aqui, oferecemos não apenas produtos de qualidade, mas também carinho, atenção e compromisso com o bem-estar do seu melhor amigo.
+
+                            Desde brinquedos e rações até acessórios e cuidados especiais, selecionamos cada item pensando no conforto e na saúde do seu pet. Nossa missão é tornar sua rotina mais prática e a vida do seu pet mais alegre.
+
+                            Porque para nós, pet é família. 💙
+                        </p>
+
+                        <ul className="space-y-4">
+                            <li className="flex items-center gap-2">
+                                <Check className="text-red-500"/>
+                                Aberto desde 2019
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <Check className="text-red-500"/>
+                                Equipe especializada!
+                            </li>
+                            <li className="flex items-center gap-2">
+                                <Check className="text-red-500"/>
+                                Qualidade profissional!
+                            </li>
+                        </ul>
+
+                        <div className="flex gap-2">
+                            <a 
+                            href="#"
+                            className="bg-[#264653] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"    
+                                > 
+                                <WhatsappLogo className="w-5 h-5 text-white"/>
+                                Contato via Whatsapp
+                            </a>
+                            <a 
+                            href="#"
+                            className="flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"    
+                                > 
+                                <MapPin className="w-5 h-5 text-black"/>
+                                Endereço da loja
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    )
+}
---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,76 @@
+import { WhatsappLogoIcon } from '@phosphor-icons/react/dist/ssr'
+import dogHeaderImg from '../../../public/Dog paw-rafiki.svg'
+import Image from 'next/image'
+import { Poppins } from 'next/font/google'
+
+const PoppinsFont = Poppins({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+export function Hero() {
+    return(
+        <section className={`bg-[#264653] text-white relative overflow-hidden ${PoppinsFont.className}`}>
+            
+            <div>
+                <Image
+                    src={dogHeaderImg}
+                    alt='Foto do cachorro'
+                    fill
+                    sizes="100vw"
+                    priority
+                    className='object-cover opacity-60 lg:hidden'
+                />
+                <div className='absolute inset-0 bg-black opacity-40 md:hidden'></div>
+            </div>
+
+            <div className="container mx-auto pt-16 pb-16 md:pb-0 px-4 relative">
+
+                <article className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
+
+                    <div className='space-y-6'>
+
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10">Seu pet merece cuidado, carinho e atenção especial.</h1>
+                        <p className="lg:text-lg">Oferecemos os melhores serviços para garantir o bem-estar e a felicidade do seu amiguinho de quatro patas</p>
+
+                        <div>
+
+                            <a href="#" 
+                                className="bg-green-500 px-5 py-2 rounded-md font-semibold 
+                                flex items-center justify-center w-fit gap-2">
+                                <WhatsappLogoIcon className='w-S h-5'/>
+                                Contato via WhatsApp!
+                            </a>
+
+                        </div>
+
+                        <div className="mt-8">
+
+                            <p className="text-sm mb-4">
+                                <b className="bg-black text-white px-2 py-1 rounded-md">5%</b> de desconto na primeira compra!
+                            </p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="hidden md:block h-full relative">
+
+                        <Image
+                            src={dogHeaderImg}
+                            alt='Foto Dog Header'
+                            className="object-contain"
+                            fill
+                            sizes="(max-width: 768px) 0vw, 50vw"
+                            quality={100}
+                            priority
+                        />
+
+                    </div>
+
+                </article>
+            </div>
+
+        </section>
+    )
+}
